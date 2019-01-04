b0c3b98320092d51a1a588ab774b21a6aa585069
unknown <rakeenali20@gmail.com>
2019-01-04 19:10:12 +0500

    added updates and set port number

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
-const webpack = require('webpack');
-const MiniCssExtractPluin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const MiniCssExtractPluin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPluin.loader,
-          'css-loader'
-        ]
+        use: [MiniCssExtractPluin.loader, "css-loader"]
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
     new MiniCssExtractPluin()
   ],
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
   },
+
   devServer: {
-    contentBase: './dist',
+    port: 3000,
+    contentBase: "./dist",
     hot: true
   }
 };
